d73ea168ee7e0c9909513577c52c42c4d49a94fb
FranziGrieser <franzi.grieser@gmail.com>
2018-11-12 12:42:42 +0100

    devise action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Devise - generate right URL inside emails from development
+  config.action_mailer.default_url_options = { host: 'newblogfn.herokuapp.com' }
 end
