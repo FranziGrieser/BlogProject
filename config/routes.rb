8a0d620573db9846872f488478533b6eb51cb0ba
FranziGrieser <franzi.grieser@gmail.com>
2018-11-12 13:04:32 +0100

    devise map login/ logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
   resources :users
   root 'posts#index'
-  resources :posts 
+  resources :posts
   get 'pages/index'
   get 'pages/about'
   get 'pages/contact'
