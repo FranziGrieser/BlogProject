2a9857ba9eea93f03435fbd4e77489d19f609d22
FranziGrieser <franzi.grieser@gmail.com>
2018-11-12 12:28:15 +0100

    create user scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_07_150840) do
+ActiveRecord::Schema.define(version: 2018_11_12_112705) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2018_11_07_150840) do
     t.text "content"
     t.string "image"
     t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "user_name"
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
