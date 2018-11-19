bf826fd52c97f06bbb59997ff3f5a59a0198ded6
FranziGrieser <franzi.grieser@gmail.com>
2018-11-19 17:54:33 +0100

    contact mailer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,15 @@ class PagesController < ApplicationController
 
   def contact
   end
+
+  def thank_you
+    @name = params[:name]
+    @subject = params[:subject]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+      to: 'franzi.grieser@yahoo.de',
+      subject: @subject,
+      body: @message).deliver_now
+  end
 end
