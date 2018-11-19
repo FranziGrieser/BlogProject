7f5ee11510b939b9d5bacbf9bd91e1a238f5fef4
FranziGrieser <franzi.grieser@gmail.com>
2018-11-19 18:38:23 +0100

    preview contact form

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-
+  def contact_form
+    UserMailer.contact_form("Franzi", "franzi@juchuu.com", "this is a subject", "Text Text Text")
+  end
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,7 +1,3 @@
 require 'test_helper'
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerTest < ActionMailer::TestCase
-  def contact_form
-    UserMailer.contact_form("Franzi", "this is a subject", "franzi@juchuu.com")
-  end
 end
