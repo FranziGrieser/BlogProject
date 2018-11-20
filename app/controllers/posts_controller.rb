ec6ef71e33e7358fc337735bd8036cea331668fd
FranziGrieser <franzi.grieser@gmail.com>
2018-11-20 12:16:01 +0100

    update set_post action with user_id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -61,6 +61,7 @@ class PostsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post= Post.find(params[:id])
+      @post.user_id = current_user.id
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
