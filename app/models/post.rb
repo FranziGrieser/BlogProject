654538ac3452a199c5b6a259c9d980cc0edc84a2
FranziGrieser <franzi.grieser@gmail.com>
2018-12-09 19:48:41 +0100

    has_many users

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :category
-  has_many :user
+  has_many :users
   has_many :comments, dependent: :destroy
-  validates :title, :content, :image, :category_id, presence: true
+  validates :title, :content, :category_id, presence: true
 end
